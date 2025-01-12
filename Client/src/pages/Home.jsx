f188e2747b47ffb595d01f69adec538c08f0cb30
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-01-12 22:08:31 +0530

    initial commit

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect}from 'react';
+
+import {Loader, Card, FormField} from '../Componenets'; // Ensure these components are properly defined
+
+const Home = () => {
+ 
+
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
