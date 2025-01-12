2cc9bb623250d7f955d2f6cd5b6eb07e14390f35
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-01-12 22:24:09 +0530

    second commit

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -3,8 +3,7 @@ import React, {useState, useEffect}from 'react';
 import {Loader, Card, FormField} from '../Componenets'; // Ensure these components are properly defined
 
 const Home = () => {
-  const []
-
+  
   return (
     <div>
       Home
