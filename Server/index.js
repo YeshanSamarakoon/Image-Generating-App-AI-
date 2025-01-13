f47a97ab41fd040b9b9d9906ecfa51fd8e9bbd41
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-01-13 12:46:50 +0530

    seventh commit

--- Server/index.js
+++ Server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './Routes/postRoutes.js';
+import dalleRoutes from './Routes/dalleRoutes.js';
 
-dotenv.config();
+dotenv.config()
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async(req, res) => {
     res.send('Greating from DALL-E!');
